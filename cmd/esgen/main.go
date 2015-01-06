46edd3551340bbac43c2350e4b658aae325a32d2
yosssi <yoshida.keiji.84@gmail.com>
2015-01-06 12:57:11 +0900

    Update cmd/esgen/main.go

--- cmd/esgen/main.go
+++ cmd/esgen/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
 	"io/ioutil"
+	"os"
 )
+
+// property represents property for each field.
+type property struct {
+	Type   string
+	Length int
+	Prefix string
+	Value  string
+}
+
+// config represents configuration for the processing.
+type config struct {
+	Action string
+	Index  string
+	Type   string
+	Num    int
+	Props  map[string]property
+}
 
 // Flags
 var (
@@ -22,5 +40,38 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(string(in))
+	var conf config
+	if err := json.Unmarshal(in, &conf); err != nil {
+		panic(err)
+	}
+
+	f, err := os.Create(*outPath)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	for i := 0; i < conf.Num; i++ {
+		meta := make(map[string]string)
+
+		meta["_index"] = conf.Index
+		meta["_type"] = conf.Type
+		meta["_id"] = conf.Props["_id"].Value
+
+		action := map[string]map[string]string{
+			conf.Action: meta,
+		}
+
+		out, err := json.Marshal(action)
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := f.Write(out); err != nil {
+			panic(err)
+		}
+
+		f.WriteString("\n")
+	}
 }
