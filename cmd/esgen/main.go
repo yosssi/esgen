3b006e2b89c4100df8ca74e085a9a877c899c8e8
yosssi <yoshida.keiji.84@gmail.com>
2015-01-08 17:00:26 +0900

    Update cmd/esgen/main.go

--- cmd/esgen/main.go
+++ cmd/esgen/main.go
@@ -218,7 +218,7 @@ func main() {
 
 			strSeq := strconv.Itoa(seqFile)
 
-			f, err = os.Create(*outPath + "." + strings.Repeat("0", 2-len(strSeq)) + strSeq)
+			f, err = os.Create(*outPath + "." + strings.Repeat("0", 4-len(strSeq)) + strSeq)
 			if err != nil {
 				panic(err)
 			}
